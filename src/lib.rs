79d130b0a5a1b9e2bc5bd1a4fbcd7f8387bc558c
Frank Lyaruu <f.lyaruu@quin.nl>
2023-07-20 01:33:49 +0200

    code golf

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ use spin_sdk::{
     http::{Request, Response},
     http_component,
 };
-use surrealdb_http::surreal::SurrealStatementReply;
-
 #[derive(Serialize,Deserialize,Debug)]
 struct Actor {
     first_name: String,
@@ -23,20 +21,17 @@ fn handle_surrealdb_spin(req: Request) -> Result<Response> {
         .get("spin-path-info")
         .ok_or(anyhow::anyhow!("missing path"))?
         .to_str()?
-        .chars()
-        .skip(1)
-        .collect::<String>()
+        [1..]
         .parse()?;
     let mut surreal_client = surrealdb_http::surreal::SurrealDbClient::new("root", "root", "http://localhost:8000", "myns", "mydb", SimpleHttpClientSpin::new_spin());
-    let mut actor: SurrealStatementReply<Actor> = surreal_client
-        .query_single(&format!("SELECT *,->played_in->film.title as films FROM actor WHERE id=actor:{}",id)).unwrap();
-    let actor_result = actor.result.pop()
+    let actor: Actor = surreal_client
+        .query_single(&format!("SELECT *,->played_in->film.title as films FROM actor WHERE id=actor:{}",id)).unwrap()
+        .result
+        .pop()
         .ok_or(anyhow!("unknown id"))?;
-
-    let ll = Bytes::from(serde_json::to_vec(&actor_result).unwrap());
     
     Ok(http::Response::builder()
         .status(200)
         .header("Content-Type", "application/json")
-        .body(Some( ll))?)
+        .body(Some(Bytes::from(serde_json::to_vec(&actor)?)))?)
 }
